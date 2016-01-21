552c8591ac3b5094b2f27c68f3ba79573d8fafe1
Ronald Chacon <ronald.chacon87@gmail.com>
2016-01-20 20:15:23 -0800

    Add angular; add angular to html, display dummy data on search

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'customers/index'
-
+  get 'angular_test', to: "angular_test#index"
   resources :customers, only: [:index]
   devise_for :users
   root 'dashboard#index'
