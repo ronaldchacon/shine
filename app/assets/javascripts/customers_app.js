0409e380ca91baf5f14a16d03d59534cef5c59da
Ronald Chacon <ronald.chacon87@gmail.com>
2016-01-23 09:35:45 -0800

    Add ngResource, use $resource for api calls. bind customer_detail to custoerdetail ctrl

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -1,4 +1,4 @@
-var app = angular.module('customers', ['ngRoute', 'templates']);
+var app = angular.module('customers', ['ngRoute', 'ngResource', 'templates']);
 
 app.config(["$routeProvider", function($routeProvider) {
   $routeProvider.when("/", {
@@ -44,14 +44,10 @@ app.controller('CustomerSearchController', ['$scope', '$http', '$location', func
   };
 }]);
 
-app.controller("CustomerDetailController", ["$scope", "$http", "$routeParams", function($scope, $http, $routeParams) {
+app.controller("CustomerDetailController", ["$scope", "$http", "$routeParams",
+"$resource", function($scope, $http, $routeParams, $resource) {
   var customerId = $routeParams.id;
-  $scope.customer = {};
+  var Customer = $resource("/customers/:customerId.json");
 
-  $http.get('/customers/' + customerId + '.json')
-    .then(function(response) {
-      $scope.customer = response.data;
-    }, function(response) {
-      alert("There was a problem: " + response.status);
-    });
+  $scope.customer = Customer.get({ "customerId": customerId });
 }]);
--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -23,6 +23,7 @@ class CustomersController < ApplicationController
 
   def show
     customer_detail = CustomerDetail.find(params[:id])
+    sleep 5
     respond_to do |format|
       format.json { render json: customer_detail }
     end
